eeb14ff022b5f28e040a9e8d73d801fdf3c32901
Jérôme Dumas <jerome.dumas@peekin.me>
2021-07-09 11:04:13 +0200

    fix index

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 export * from './gcloud-pubsub.module';
 export * from './interfaces/gcloud-pubsub-async-options.interface';
 export * from './interfaces/gcloud-pubsub-options.interface';
-export * from './microservice/gcloud-pubsub.service';
+export * from './microservice/gcloud-pubsub-server';
